1a2ceb1685be25d71d8830afe3870b116f7659dc
dev-gabrielchaves <dev.gchaves@gmail.com>
2023-11-06 12:19:08 -0300

    Added flash messages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, flash
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -12,7 +12,9 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # Note that you don’t have to pass request.form to Flask-WTF, it will load automatically. And the convenient validate_on_submit will check if it is a POST request and if it is valid.
-        return redirect(url_for('home'))
+        # The flashing system basically makes it possible to record a message at the end of a request and access it next request and only next request.
+        flash("You've been registered successfully!") # So here I've set a flash message, and that message will just be used for the next request
+        return redirect(url_for('login'))
     # print(form.errors) -> Just for testing, in case of a wrong password it will show: {'confirm_password': ['Field must be equal to password.']}
     return render_template('register.html', form=form)
 
@@ -20,6 +22,7 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        flash("You've been logged in successfully!")
         return redirect(url_for('home'))
     return render_template('login.html', form=form)
 
