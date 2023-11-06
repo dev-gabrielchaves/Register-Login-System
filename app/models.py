d86ec430ff9bab9d13902ff7af9151321b09e6fb
dev-gabrielchaves <dev.gchaves@gmail.com>
2023-11-06 15:22:07 -0300

    Turned application into a package
    Turn the application into a package makes it easier to work with, it is also good practice to separate our code into different modules. I've also added requirements file

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+# SQLAlchemy allow us to work with classes when setting our tables to the database
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"User('{self.username}', '{self.email}')"
--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, flash
-from forms import RegistrationForm, LoginForm
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'This Is The Secret Key'
-# Setting the URI where our database will be located
-# In the case of using a sqlite database, the structure is like the following
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
-db = SQLAlchemy(app) # Creating an object db, instace of the class SQLAlchemy, that will allow us to work with the database
-app.app_context().push()
-
-# SQLAlchemy allow us to work with classes when setting our tables to the database
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self) -> str:
-        return f"User('{self.username}', '{self.email}')"
+from flask import render_template, redirect, url_for, flash
+from app import app
+from app.forms import RegistrationForm, LoginForm
 
 @app.route('/')
 def home():
@@ -42,6 +24,3 @@ def login():
         flash("You've been logged in successfully!")
         return redirect(url_for('home'))
     return render_template('login.html', form=form)
-
-if __name__ == '__main__':
-    app.run(debug=True)
